c4468ffd138792b1e8553efe9e08977ebbf86323
carlos <carloscarmonaalcantara@gmail.com>
2017-04-28 11:38:44 +0200

    Simplify executor development

--- index.js
+++ index.js
@@ -11,38 +11,26 @@ class pdfExecutor extends Execution {
     super(process);
   }
 
-  exec() {
+  exec(options) {
     var _this = this;
-    return new Promise(function (resolve, reject) {
-      _this.getValues()
-        .then((res) => {
-          var options = res;
-          if(res.typeoutput){
-            options.type = res.typeoutput;
-          }
 
-          var html = fs.readFileSync(options.html, 'utf8');
-          if(options.args){
-            html = ejs.render(html,options.args);
-          }
+    var html = fs.readFileSync(options.html, 'utf8');
+    if(options.args){
+      html = ejs.render(html,options.args);
+    }
 
-          pdf.create(html, options).toFile(options.output, function(err, res) {
-            if (err) {
-              var endOptions = {
-                end: 'error',
-                messageLog: `PDF Executor: ${err}`,
-                execute_err_return: `PDF Executor: ${err}`,
-                execute_return: ''
-              };
-              _this.end(endOptions, resolve, reject);
-            }else{
-              var endOptions = {
-                end: 'end'
-              };
-              _this.end(endOptions, resolve, reject);
-            }
-          });
-        });
+    pdf.create(html, options).toFile(options.output, function(err, res) {
+      if (err) {
+        var endOptions = {
+          end: 'error',
+          messageLog: `PDF Executor: ${err}`,
+          execute_err_return: `PDF Executor: ${err}`,
+          execute_return: ''
+        };
+        _this.end(endOptions);
+      }else{
+        _this.end();
+      }
     });
   }
 }
